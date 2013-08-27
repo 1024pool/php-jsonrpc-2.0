f9f984c993f772f0bbd43ec1826b28fdd40fdaa5
xjmarui <xjmarui@163.com>
2013-08-27 21:29:13 +0800

    init

---
+++ api.php
@@ -0,0 +1,209 @@
+<?php
+
+class JSONRPC_Exception extends Exception {
+
+    const STATUS_PARSE_ERROR = -32700;
+    const STATUS_INVALID_REQUEST = -32600;
+    const STATUS_METHOD_NOT_FOUNT = -32601;
+    const STATUS_INVALID_PARAMS = -32602;
+    const STATUS_INTERNAL_ERROR = -32603;
+
+    static $errors = array(
+        -32700=> 'Parse error',
+        -32600=> 'Invalid Request',
+        -32601=> 'Method not found',
+        -32602=> 'Invalid params',
+        -32603=> 'Internal error',
+    );
+
+
+    public function __construct($error_code) {
+
+        if (!array_key_exists($error_code, self::$errors)) {
+
+            $error_code = self::STATUS_INTERNAL_ERROR;
+        }
+
+        parent::__construct(self::$errors[$error_code], $error_code);
+    }
+}
+
+class JSONRPC {
+
+    //request的json数据
+    public $request;
+
+    //分析获取后的method
+    public $method;
+
+    //分析获取后的params
+    public $params;
+
+    //分析获取的id
+    public $id;
+
+    //初始化
+    public function __construct($request = NULL) {
+
+        if (!$request) {
+            //不设定request，则默认从php输入流中获取
+            $this->request= file_get_contents('php://input');
+        }
+        else {
+            $this->request = $request;
+        }
+
+        if (isset($_COOKIE['session_id'])) {
+            session_id($_COOKIE['session_id']);
+        }
+        else {
+            setcookie('session_id', session_id());
+        }
+
+        session_start();
+
+    }
+
+    public function run() {
+
+        //无论是否有错误，都发送头进行返回
+        $this->send_header();
+
+        try {
+
+            //进行request分析
+            $this->process();
+
+            //获取已绑定_functions
+            $_functions = $this->_functions;
+
+            //遍历判定是否包含可用method
+            if (array_key_exists($this->method, $_functions)) {
+                $this->method = $_functions[$this->method];
+            }
+            else {
+                throw new JSONRPC_Exception(JSONRPC_Exception::STATUS_METHOD_NOT_FOUNT);
+            }
+
+            $method = $this->method;
+            $params = $this->params;
+
+            if ((is_string($method) && !function_exists($method))
+                    ||
+                    (is_array($method) && !method_exists($method[0], $method[1]))
+               ) throw new JSONRPC_Exception(JSONRPC_Exception::STATUS_METHOD_NOT_FOUNT);
+
+            //调用，获取结果
+            $this->result = call_user_func_array($method, $params);
+
+            //发送结果
+            $this->send_body();
+
+        }
+        catch(JSONRPC_Exception $e) {
+
+            //抓到异常发送错误
+            $this->send_error($e->getCode(), $e->getMessage());
+        }
+    }
+
+    private $_functions = array();
+
+    //进行绑定
+    public function expose($origin, $dest = NULL) {
+
+        if ($dest === NULL) {
+            $dest = $origin;
+        }
+
+        $this->_functions[$origin] = $dest;
+
+    }
+
+    //进行request解析
+    public function process() {
+
+        //decode
+        $decoded_array = json_decode($this->request, TRUE);
+
+        if (json_last_error()) {
+
+            //出现错误抛出语法异常错误
+            throw new JSONRPC_Exception(JSONRPC_Exception::STATUS_PARSE_ERROR);
+        }
+
+        if (!isset($decoded_array['method'])) {
+            //抛出不可用请求错误
+            throw new JSONRPC_Exception(JSONRPC_Exception::STATUS_INVALID_REQUEST);
+        }
+        else {
+            $this->method = $decoded_array['method'];
+        }
+
+        if (!isset($decoded_array['params'])) {
+            //抛出参数错误
+            throw new JSONRPC_Exception(JSONRPC_Exception::STATUS_INVALID_PARAMS);
+        }
+        else {
+            $this->params = $decoded_array['params'];
+        }
+
+        if (!isset($decoded_array['id'])) {
+            //抛出错误情趣
+            throw new JSONRPC_Exception(JSONRPC_Exception::STATUS_INVALID_REQUEST);
+        }
+        else {
+            $this->id = $decoded_array['id'];
+        }
+    }
+
+    public function send_header() {
+        //header表明json
+        header('Content-Type: application/json');
+    }
+
+    public function send_body() {
+
+        //正常结果返回
+        $array = array(
+                'jsonrpc'=> '2.0',
+                'result'=> $this->result,
+                'id'=> $this->id
+                );
+
+        echo json_encode($array);
+    }
+
+    public function send_error($code, $message) {
+
+        //发送错误返回
+        $array = array(
+                'jsonrpc'=> '2.0',
+                'error'=> array(
+                    'code'=> $code,
+                    'message'=> $message
+                    ),
+                'id'=> $this->id
+                );
+        echo json_encode($array);
+    }
+
+    public function __destruct() {
+        session_write_close();
+    }
+}
+
+function hello() {
+    return 'hello';
+}
+
+//初始化jsonrpc对象
+$jsonrpc = new JSONRPC;
+
+//进行调用方法并绑定，左侧调用方法，右侧为实际调用方法
+//支持绑定到对象方法
+$jsonrpc->expose('trim');
+$jsonrpc->expose('hello', 'hello');
+
+//执行
+$jsonrpc->run();
---
+++ rpc.php
@@ -0,0 +1,86 @@
+<?php
+
+class Cookie_File {
+
+    public $path;
+
+    function __construct() {
+        $this->path = tempnam(sys_get_temp_dir(), 'rpc.cookie.');
+        @touch($this->path);
+    }
+
+    function __destruct() {
+        unlink($this->path);
+    }
+
+}
+
+class RPC_Exception extends Exception {}
+
+class RPC {
+
+    public $url, $cookie_file;
+    private $_id;
+
+    public function __construct($url) {
+        $this->url = $url;
+        $this->_id = uniqid();
+        $this->cookie_file = new Cookie_File;
+    }
+
+    public function __call($method, $params = array()) {
+        return call_user_func_array(array($this, 'call'), array(
+            $method,
+            $params
+        ));
+    }
+
+    public function call($method, $params) {
+        $ch = curl_init();
+
+        curl_setopt_array($ch, array(
+            CURLOPT_RETURNTRANSFER => TRUE,
+            CURLOPT_COOKIEJAR => $this->cookie_file->path,
+            CURLOPT_COOKIEFILE => $this->cookie_file->path,
+            CURLOPT_URL =>  $this->url,
+            CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9.1.13) Gecko/20100914 Firefox/3.5.13( .NET CLR 3.5.30729)',
+            CURLOPT_POST => TRUE,
+            CURLOPT_CONNECTTIMEOUT=> 5,
+            CURLOPT_POSTFIELDS => json_encode(array(
+                'jsonrpc'=> '2.0',
+                'method'=> $method,
+                'params'=> $params,
+                'id'=> $this->_id
+            ))
+        ));
+
+        $result = json_decode(curl_exec($ch), TRUE);
+
+        curl_close($ch);
+
+        if (!isset($result['jsonrpc']) || $result['jsonrpc'] != '2.0') {
+            throw new RPC_Exception('Wrong Response ! It`s Not JSONRPC2.0');
+        }
+
+        if ($result['id'] != $this->_id) {
+            throw new RPC_Exception('Wrong Response Id');
+        }
+
+        if (isset($result['error'])) {
+            throw new RPC_Exception($result['error']['message']);
+        }
+
+        return $result['result'];
+    }
+}
+
+$rpc = new RPC('http://rpc/api.php');
+
+try {
+    echo $rpc->hello(). "\n";
+    echo $rpc->trim("\t\n hello "). "\n";
+    echo $rpc->create_exception();
+}
+catch(RPC_Exception $e) {
+    echo $e->getMessage();
+}
